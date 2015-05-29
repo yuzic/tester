242740d724d3e9f4badb507a9532c0c271081e3f
itcoder <you@example.com>
2015-05-29 18:44:32 +0300

    fixing readme

--- acceptance/regforum/indexTest.php
+++ acceptance/regforum/indexTest.php
@@ -1,8 +1,10 @@
 <?php
 $tester = new \Tester();
+// утсновка параметров из конфигурационного файла
 $tester->setUrl(Config::get('regforum'));
+// утсановка теcтируемого url
 $tester->send('/', []);
-//
+// смотрим на текст на странице
 $tester->seeText('Добро пожаловать на Регфорум');
 $tester->seeText('Зарегистрироваться на Регфоруме');
 $tester->seeText('Форум');
